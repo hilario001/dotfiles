9ede4999db3e39dfc6e67aeffe760274b54d96dd
voizd <voizd00@gmail.com>
2025-03-08 19:47:06 +0800

    deleted deploy.sh

--- .config/zsh/.zshenv
+++
@@ -1,6 +0,0 @@
-export ZDOTDIR="$HOME/.config/zsh"
-export KEYTIMEOUT=5
-export EDITOR=vim
-export BROWSER=firefox
-export PATH=$PATH:~/.local/bin:/usr/lib/jvm/java-23-openjdk
-export JAVA_HOME=/usr/lib/jvm/java-23-openjdk
--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
-loc="$HOME/.config/";
-for file in $(pwd)/.dotfiles/.config/*; do
-#ln -sv $file $loc;
-    printf "%s\n" "$file";
-done
+from="$(pwd)/.dotfiles/.*";
+to="$HOME";
+ln -sv $from $to;
+
+#loc="$HOME/.config/";
+#for file in $(pwd)/.dotfiles/.config/*; do
+#    ln -sv $file $loc;
+#done

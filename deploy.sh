c20b24cf347edba92eec9986a1df888baf001294
voizd <voizd00@gmail.com>
2025-03-08 19:48:41 +0800

    deleted deploy.sh

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-from="$(pwd)/.dotfiles/.*";
-to="$HOME";
-ln -sv $from $to;
-
-#loc="$HOME/.config/";
-#for file in $(pwd)/.dotfiles/.config/*; do
-#    ln -sv $file $loc;
-#done
